c81f1b96b4eb65178c7f7047d77a2accddcf1382
jasmine arabgard <jasmine.arabgard@outlook.com>
2025-06-13 23:31:55 +0330

    add item model

---
+++ Models/Item.cs
@@ -0,0 +1,13 @@
+﻿namespace MYShop.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        public Product Product { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int QuntityInStuck{  get; set; }
+    }
+}
