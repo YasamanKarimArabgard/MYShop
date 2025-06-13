d7d2b82cb9b2cfd1fb35d28189e73728f916ffa4
jasmine arabgard <jasmine.arabgard@outlook.com>
2025-06-13 23:31:13 +0330

    add cartitem model

---
+++ Models/CartItem.cs
@@ -0,0 +1,23 @@
+﻿namespace MYShop.Models
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+
+        public Item Item { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal GetTotalPrice()
+        {
+            if (Item != null && Item.Price > 0 && Quantity > 0)
+            {
+                return Item.Price * Quantity;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+                                    }
+}
