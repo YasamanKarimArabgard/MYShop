1706dd7e436706707d1b5b1698f5bf9a6688eb98
jasmine arabgard <jasmine.arabgard@outlook.com>
2025-06-13 23:30:59 +0330

    add cart model

---
+++ Models/Cart.cs
@@ -0,0 +1,35 @@
+﻿namespace MYShop.Models
+{
+    public class Cart
+    {
+        public Cart()
+        {
+            CartItems = new List<CartItem>();
+        }
+        public int OrderId { get; set; }
+
+        public List<CartItem> CartItems { get; set; }
+
+        public void AddItem(CartItem item)
+        {
+            if (CartItems.Exists(i => i.Item.Id == item.Item.Id))
+            {
+                CartItems.Find(i => i.Item.Id == item.Item.Id)
+                    .Quantity += 1;
+            }
+            else
+            {
+                CartItems.Add(item);
+            }
+        }
+
+        public void RemoveItem(CartItem item) { 
+        
+           if(CartItems.Exists(i => i.Item.Id == item.Item.Id))
+            {
+               
+            }
+
+        }
+    }
+}
