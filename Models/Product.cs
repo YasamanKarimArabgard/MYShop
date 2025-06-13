1c79a097a1581b3c71b4bc73dc705aeeee19fe8b
jasmine arabgard <jasmine.arabgard@outlook.com>
2025-06-13 23:32:09 +0330

    add product model

--- Models/Product.cs
+++ Models/Product.cs
@@ -2,5 +2,17 @@
 {
     public class Product
     {
-    }
+
+        public Product()
+        {
+            categories = new List<Category>();  
+        }
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public List<Category> categories { get; set; }
+     }
 }
